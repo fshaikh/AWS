4a93a54f8ef33f2907c451a7e4ab9cd2efefd6be
Furqan Shaikh <furqan_shaikh_1999@hotmail.com>
2016-11-09 22:36:12 +0530

    Added a reference name example

--- AWS/S3Utility.js
+++ AWS/S3Utility.js
@@ -10,6 +10,9 @@ module.exports = (function () {
         // 1. Generates a random number between 1 and <length>.Each number is converted to hex and added to an array.
         // 2. All array elements are concateneted using join function
         // 3. 
+        
+        // https://s3.amazonaws.com/bucket_name/user_media/videos/screenshots/cmXRyLRQxe9R139023426817_vid001.jpeg
+        //where the prefix: cmXRyLRQxe9R139023426817_ is a self generated string we build and concat to the original file name: vid001.jpg before uploading to s3.
         getRandomKeyName: function (length){
             var result = [];
             var min = 1;
